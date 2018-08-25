6eee2a2e40fdc9829cecadce896544981ede884b
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2018-08-25 15:01:17 +0300

    Fix config

--- config.sh
+++ config.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eux
 
-mkdir /etc/letsencrypt
-
 aws s3 cp ${S3} /etc/letsencrypt --recursive
 
 [ `ls -1A . | wc -l` -eq 0 ] && certbot register --agree-tos --register-unsafely-without-email
