9d255239171e61b2e5e57caf3d97c782c5caf8a7
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2018-08-25 15:29:54 +0300

    test lifecycle

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eux
-
-aws s3 cp ${S3} /etc/letsencrypt --recursive
-
-if [ `ls -1A /etc/letsencrypt | wc -l` -eq 0 ] ;
-then
-   certbot register --agree-tos --register-unsafely-without-email
-fi
--- publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eux
-
-aws s3 cp /etc/letsencrypt ${S3} --recursive
